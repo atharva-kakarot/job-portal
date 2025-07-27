e4e8e7a453a01e8d594c82ff969540a465364c4f
atharva-kakarot <atharva.kakarot@gmail.com>
2025-07-27 17:08:29 +0530

    Added registerCompany, getCompany and getCompanyById functions

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -18,5 +18,52 @@ export const registeredCompany = async (req, res) => {
         success: false,
       });
     }
-  } catch (error) {}
+
+    company = await Company.create({
+      name: companyName,
+      userId: req.id,
+    });
+
+    return res.status(200).json({
+      message: "Company has been created successfully!",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCompany = async (req, res) => {
+  try {
+    const userId = req.id;
+    const companies = await Company.find({ userId });
+    if (!companies) {
+      return res.status(404).json({
+        message: "Company not found",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCompanyById = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found.",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
