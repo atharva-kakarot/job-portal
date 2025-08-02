129713788e634a82b280b22431e0204363042266
atharva-kakarot <atharva.kakarot@gmail.com>
2025-08-02 16:00:35 +0530

    Added company routes

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,6 +1,6 @@
 import { Company } from "../models/company.model.js";
 
-export const registeredCompany = async (req, res) => {
+export const registerCompany = async (req, res) => {
   try {
     const { companyName } = req.body;
     if (!companyName) {
@@ -12,7 +12,7 @@ export const registeredCompany = async (req, res) => {
 
     let company = await Company.findOne({ name: companyName });
 
-    if (!company) {
+    if (company) {
       return res.status(400).json({
         message: "You can't add the same company",
         success: false,
@@ -63,6 +63,32 @@ export const getCompanyById = async (req, res) => {
       company,
       success: true,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateCompany = async (req, res) => {
+  try {
+    const { name, description, website, location } = req.body;
+    const file = req.file;
+
+    const updateData = { name, description, website, location };
+    const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
+      new: true,
+    });
+
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found.",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Company informatiion is updated",
+      success: "true"
+    })
   } catch (error) {
     console.log(error);
   }
