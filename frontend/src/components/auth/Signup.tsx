888f4d7f4961a7d94331e8ea0bbd948ad55d7d6e
atharva-kakarot <atharva.kakarot@gmail.com>
2025-08-12 12:52:53 +0530

    Added signup form

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -1,9 +1,54 @@
 import Navbar from "../shared/Navbar";
+import { Input } from "../ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const Signup = () => {
   return (
     <div>
       <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          action=""
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+          <div className="my-2">
+            <label>Full name</label>
+            <Input type="text" placeholder="Enter your full name" />
+          </div>
+          <div className="my-2">
+            <label>Email</label>
+            <Input type="text" placeholder="Enter your email" />
+          </div>
+          <div className="my-2">
+            <label>Phone number</label>
+            <Input type="text" placeholder="Enter your phone number" />
+          </div>
+          <div className="my-2">
+            <label>Password</label>
+            <Input type="text" placeholder="Enter your password" />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="default" id="r1" />
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="comfortable" id="r2" />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
