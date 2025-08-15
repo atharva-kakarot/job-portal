055e266f4b2046630835fa88c8a46a1cc9f046d6
atharva-kakarot <atharva.kakarot@gmail.com>
2025-08-15 17:02:40 +0530

    Added hero section

---
+++ frontend/src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+const HeroSection = () => {
+  return (
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10">
+        <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#f83002] font-medium">
+          No.1 Job Hunting Website
+        </span>
+        <h1 className="text-5xl font-bold">
+          Search, Apply, & <br />
+          Get your <span className="text-[#6A38C2]">Dream Jobs</span>{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,14 @@
+import HeroSection from "./HeroSection";
 import Navbar from "./shared/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection/>
+      {/* <CategoryCarousel/>
+      <LatestJobs/>
+      <Footer/> */}
     </div>
   );
 };
