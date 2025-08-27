955fc76fe991a7d68253c4ae3fbcae81f0d0b67b
atharva-kakarot <atharva.kakarot@gmail.com>
2025-08-27 19:12:28 +0530

    Updated CSS of Job card in Job.tsx

--- frontend/src/components/Job.tsx
+++ frontend/src/components/Job.tsx
@@ -5,13 +5,20 @@ import { AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { useNavigate } from "react-router-dom";
 import type { Job } from "@/redux/jobSlice";
+import TimeAgo from "react-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+import TimeAgoLib from "javascript-time-ago";
+
+TimeAgoLib.addDefaultLocale(en);
 
 const JobCard = ({ job }: { job: Job }) => {
   const navigate = useNavigate();
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex items-center justify-between">
-        <p className="text-sm text-gray-500">2 days ago</p>
+        <p className="text-sm text-gray-500">
+          <TimeAgo date={new Date(job.createdAt)} />
+        </p>
         <Button variant="outline" className="rounded-full" size="icon">
           <Bookmark />
         </Button>
@@ -29,11 +36,9 @@ const JobCard = ({ job }: { job: Job }) => {
       </div>
       <div>
         <h1 className="font-bold text-lg my-2">{job.title}</h1>
-        <p className="text-sm text-gray-600">
-          {job.description}
-        </p>
+        <p className="text-sm text-gray-600">{job.description}</p>
       </div>
-      <div className="flex items-center gap-2 mt-4">
+      <div className="flex items-center gap-2 mt-4 flex-wrap">
         <Badge className={"text-blue-700 font-bold mr-2"} variant="secondary">
           {job.position} positions
         </Badge>
